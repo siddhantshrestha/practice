2361fdb0caac709189e80ce893f3e047541fa60b
sid <sidstha58@gmail.com>
2020-06-17 17:19:04 +0545

    module5

--- module_4/speakgoodbye.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-    var byeSpeaker = {
-        speak: function(names) {
-            console.log(speakWord + " " + names);
-        }
-    };
-    var speakWord = "goodbye";
-    window.byeSpeaker = byeSpeaker;
-})(window);
